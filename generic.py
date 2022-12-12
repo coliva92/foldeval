ddb831679290c677666a6c4096f7e1c0ec7f6824
Carlos Oliva <47442158+coliva92@users.noreply.github.com>
2022-12-12 14:02:46 -0800

    Add files via upload

---
+++ generic.py
@@ -0,0 +1,30 @@
+from abc import ABC, abstractmethod
+
+
+
+
+
+class PredictionAlgorithm(ABC):
+  
+  def __init__(self):
+    global sequence
+    self.plddt_scores = None
+    self.tqdm_format = '{l_bar}{bar}| {n_fmt}/{total_fmt} ' + \
+                       '[elapsed: {elapsed} remaining: {remaining}]'
+
+  @abstractmethod
+  def setup(self) -> None: pass
+  
+  @abstractmethod
+  def do_msa(self) -> None: pass
+
+  @abstractmethod
+  def make_prediction(self) -> str: pass
+
+  @abstractmethod
+  def display_predicted_model(self) -> None: pass
+
+  @abstractmethod
+  def prepare_results_folder(self) -> str:
+    global jobname
+    return jobname
